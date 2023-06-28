ce1e9fbb8fb9870d2c2021eea5dc2e33d97b3827
Alessio Alfonsi <61656683+gudea31@users.noreply.github.com>
2023-06-28 14:25:35 +0200

    improved carousel for area pages

--- pages/areas/[id].vue
+++ pages/areas/[id].vue
@@ -2,21 +2,24 @@
     Page description for a single area.
 -->
 <template>
-  <main>  
-    <Carousel id="carousel" :autoplay="2000" :wrap-around="true" :items-to-show="1.1">
-      <Slide v-for="slide in slides" :key="slide">
-        <img :src="slide" class="carousel__item" :class="{ 'responsive-image': true }"/>
-      </Slide>
-      <template #addons>
-        <Pagination />
-      </template>
-    </Carousel>
+  <main> 
+    <div id="carousel-container">
+      <Carousel id="carousel" :autoplay="2000" :wrap-around="true" :items-to-show="1.1">
+        <Slide v-for="slide in slides" :key="slide">
+          <img :src="slide" class="carousel__item" :class="{ 'responsive-image': true }"/>
+        </Slide>
+        <template #addons>
+          <Pagination />
+        </template>
+      </Carousel>
+    </div>
     
     <div id="project-container">
       <CircleContainer v-for="project of data[1]" :link="'/projects/' + project.id" :title="project.name" />
     </div>
   </main>
 </template>
+
 
 <script>
     import { Carousel, Pagination, Slide } from 'vue3-carousel'
@@ -37,9 +40,9 @@
         data[1] = await $fetch(useRuntimeConfig().baseURL + '/server/projects/byarea/' + route.params.id)
       
         const slides = [
-          "/_nuxt/assets/img/food_area/food_1.jpg",
-          "/_nuxt/assets/img/food_area/food_2.jpg",
-          "/_nuxt/assets/img/food_area/food_3.jpg"
+          "/_nuxt/assets/img/food_area/bee.jpg",
+          "/_nuxt/assets/img/food_area/fresh-bread.jpg",
+          "/_nuxt/assets/img/food_area/orange.jpg"
         ]
       
         return {
@@ -52,7 +55,6 @@
 
 <style>
 
-  
 main {
   width: 100%;
   padding: 0;
@@ -63,10 +65,27 @@ main {
   overflow-x: visible;
 }
 
-#carousel{
+.breadcrump{
+  z-index: 5;
+}
+
+#carousel-container {
+  position: relative;
+  top: -55px;
+  width: 100%;
+  margin: 0;
+  align-content: center;
+  z-index: 1;
+}
+
+#carousel {
+  position: relative;
+  left: 50%;
+  transform: translateX(-50%);
   width: 120%;
   margin: 0;
   align-content: center;
+  pointer-events: none;
 }
 
 #project-container {
@@ -76,6 +95,8 @@ main {
   justify-content: center;
   align-content: center;
   gap: 40px;
+  margin-top: 20px; 
+  z-index: 2;
 }
 
 .footer {
@@ -86,7 +107,8 @@ main {
 }
 
 .carousel__item {
-  min-height: 150px; 
+  /*min-height: 150px; */
+  height: 10%;
   width: 100%;  
   background-color: var(--vc-clr-primary);
   color: var(--vc-clr-white);
@@ -110,8 +132,9 @@ main {
 
 .responsive-image {
   max-width: 100%;
-  height: 100%;
+  height: 350px;
   object-fit:contain;
+  background-color: white;
 }
 
 </style>
