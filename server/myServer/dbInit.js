2ba0ea84f59fdc8a936d8230c095d4f57ba5d956
Nolwenn <nolwenn.peyratout@etu.univ-cotedazur.fr>
2023-07-01 18:41:37 +0200

    #22 Add description for each project

--- server/myServer/dbInit.js
+++ server/myServer/dbInit.js
@@ -39,10 +39,7 @@ export default async (models) => {
     const projectModel = [
         {
             name: "Ringo",                  //1st project (food)
-            presentation: loremIpsum({
-                count: 4,
-                units: "paragraph"
-            }),
+            presentation: "Introducing the Ringo project by Barilla! Led by CEO Mario Rossi, this exciting food startup aims to produce delicious and innovative cookies. With a passion for quality and taste, Ringo combines Barilla's expertise with cutting-edge technology to create irresistible treats. Join us on this culinary journey as we redefine the cookie experience with our mouthwatering creations. Stay tuned for a delightful blend of tradition and innovation in every bite!",
             location: "Milan, Italy",
             date: "23/04/2014",
             start_up: "Barilla",
@@ -51,10 +48,7 @@ export default async (models) => {
         },
         {
             name: "Apple Visor",            //2nd project (IT)
-            presentation: loremIpsum({
-                count: 4,
-                units: "paragraph"
-            }),
+            presentation: "Revolutionizing Mobile Devices! This groundbreaking IT project, spearheaded by Apple, aims to redefine the way we experience mobile technology. The Apple Visor combines cutting-edge innovation, sleek design, and advanced features to create a truly immersive and seamless user experience. Get ready to embark on a journey where mobility meets innovation, as Apple brings its signature touch to the world of mobile devices. Stay tuned for a new era of connectivity, convenience, and unparalleled performance with the Apple Visor! ",
             location: "Cupertino, California",
             date: "15/05/2023",
             start_up: "Apple",
@@ -63,10 +57,7 @@ export default async (models) => {
         },
         {
             name: "Lidar Scanner",                //3rd project (health)
-            presentation: loremIpsum({
-                count: 4,
-                units: "paragraph"
-            }),
+            presentation: "Advancing Hospital Imaging! This state-of-the-art scanner, developed by the innovative startup Ecolab, is set to revolutionize hospital imaging technology. The Lidar Scanner combines cutting-edge lidar technology with precision and accuracy to deliver detailed and real-time imaging results. Designed with the needs of healthcare professionals in mind, this advanced scanner promises enhanced diagnostics and improved patient care. Join us on this transformative journey as Ecolab redefines hospital imaging with the Lidar Scanner. Stay tuned for a new era of medical imaging excellence! ",
             location: "Bologna, Italy",
             date: "03/12/2007",
             start_up: "Ecolab s.r.l.",
@@ -75,10 +66,7 @@ export default async (models) => {
         },
         {
             name: "BioVegan Smoothies",                //4th project (food)
-            presentation: loremIpsum({
-                count: 4,
-                units: "paragraph"
-            }),
+            presentation: "This exciting food project brings together the expertise of Valfrutta in organic and vegan products. Led by our dedicated team, we are crafting refreshing smoothies packed with natural goodness. With a focus on quality and sustainability, BioVegan Smoothies combine the best of nature's flavors and nutritional benefits. Join us on this delicious journey as we blend organic fruits and vegetables into vibrant and nourishing drinks. Sip your way to a healthier and tastier lifestyle with Valfrutta's BioVegan Smoothies! ",
             location: "Milan, Italy",
             date: "30/04/2020",
             start_up: "Valfrutta",
@@ -87,10 +75,7 @@ export default async (models) => {
         },
         {
             name: "Starlink",                //5th project (IT)
-            presentation: loremIpsum({
-                count: 4,
-                units: "paragraph"
-            }),
+            presentation: "Empowering Global Connectivity! Developed by SpaceX in California, Starlink is a groundbreaking internet system that aims to provide high-speed, reliable, and accessible internet to people worldwide. With a constellation of advanced satellites orbiting the Earth, Starlink revolutionizes connectivity, bridging the digital divide and unlocking new opportunities for communities globally. Join us on this mission to create a more connected world, where everyone can access the power of the internet, no matter where they are. Stay tuned for a future where reliable internet access knows no boundaries with Starlink! ",
             location: "Hawthorne, California",
             date: "01/11/2016",
             start_up: "SpaceX",
@@ -99,10 +84,7 @@ export default async (models) => {
         },
         {
             name: "The Next Hambuger",            //6th project (food)
-            presentation: loremIpsum({
-                count: 4,
-                units: "paragraph"
-            }),
+            presentation: "Redefining Meat with 3D Printing! This innovative project, based in Brussels, is pushing the boundaries of food technology by creating 3D-printed meat. FoodEvolution's cutting-edge approach combines sustainability, taste, and nutrition, revolutionizing the way we consume meat. With advanced 3D printing techniques, we are crafting delicious, plant-based alternatives that replicate the texture and flavor of traditional meat products. Join us on this exciting journey as we reshape the future of food, providing sustainable and mouthwatering options with The Next Hamburger by FoodEvolution. ",
             location: "Bruxelles, Belgium",
             date: "13/01/2017",
             start_up: "FoodEvolution",
@@ -111,10 +93,7 @@ export default async (models) => {
         },
         {                                       //7th project (health)
             name: "Heartless",                
-            presentation: loremIpsum({
-                count: 4,
-                units: "paragraph"
-            }),
+            presentation: "Revolutionizing Cardiac Care with 3D-Printed Heart Valves! Developed in London, this groundbreaking project is poised to transform the field of cardiology by creating innovative 3D-printed heart valves. Maven Clinic's cutting-edge technology and expertise merge to engineer personalized, high-quality heart valves that offer enhanced functionality and longevity. With a focus on precision and patient well-being, Heartless aims to improve cardiac treatment outcomes and redefine the future of cardiovascular care. Join us on this remarkable journey as we revolutionize the world of cardiology with 3D-printed heart valves by Maven Clinic. ",
             location: "London, England",
             date: "03/12/2021",
             start_up: "Maven Clinic",
@@ -123,10 +102,7 @@ export default async (models) => {
         },
         {
             name: "Glucometer",                    //8th project (health)
-            presentation: loremIpsum({
-                count: 4,
-                units: "paragraph"
-            }),
+            presentation:"Empowering Diabetic Care with Advanced Glucose Monitoring! Developed by the innovative startup Medely in Shanghai, Glucometer is a state-of-the-art glucose monitoring device designed to revolutionize diabetic care. With its cutting-edge technology and user-friendly interface, Glucometer enables accurate and convenient monitoring of blood glucose levels. This compact and portable device empowers individuals to take control of their health by providing real-time data and insights. Join us on this transformative journey as we redefine diabetic care with Glucometer by Medely. Stay tuned for a future where managing diabetes becomes easier and more efficient!",
             location: "Shanghai, China",
             date: "08/06/2015",
             start_up: "Medely",
@@ -135,10 +111,7 @@ export default async (models) => {
         },
         {
             name: "VotaMi",                //9th project (IT)
-            presentation: loremIpsum({
-                count: 4,
-                units: "paragraph"
-            }),
+            presentation: "Revolutionizing Voting with Fingerprint Technology! Developed by MHackeroni in Italy, VotaMi is an innovative fingerprint-based voting system. With advanced biometrics, VotaMi ensures secure and accurate voting, enhancing the integrity of elections. This user-friendly system allows voters to cast their ballots confidently. Join us on this transformative journey as we redefine democracy with VotaMi. Stay tuned for a future where voting is secure, accessible, and transparent for all! ",
             location: "Milan, Italy",
             date: "26/03/2022",
             start_up: "MHackeroni",
@@ -147,10 +120,7 @@ export default async (models) => {
         },
         {
             name: "Nutella",            //10th project (food)
-            presentation: loremIpsum({
-                count: 4,
-                units: "paragraph"
-            }),
+            presentation: "This delicious food project aims to produce the iconic hazelnut cream loved by millions worldwide. With a passion for quality and flavor, Ferrero S.p.a combines the finest ingredients to create the perfect blend of creamy chocolate and rich hazelnuts. Join us on this delectable journey as we spread happiness and indulge in the irresistible taste of Nutella. Stay tuned for a world of sweetness and delight with the Nutella Project by Ferrero S.p.a!",
             location: "Alba, Italy",
             date: "02/02/2011",
             start_up: "Ferrero S.p.a.",
@@ -159,10 +129,7 @@ export default async (models) => {
         },
         {
             name: "DetectionSystem",            //11th project (IT)
-            presentation: loremIpsum({
-                count: 4,
-                units: "paragraph"
-            }),
+            presentation: "This cutting-edge IT project aims to revolutionize facial recognition technology with its advanced Face Detector. Powered by state-of-the-art algorithms and machine learning, our system offers highly accurate and efficient face detection capabilities. Whether for security, identification, or enhancing user experiences, DetectionSystem is set to redefine how we interact with facial recognition technology. Join us on this transformative journey as we unlock new possibilities and drive innovation with the DetectionSystem project by NewWave Corporation.",
             location: "Tokyo, Japan",
             date: "31/08/2019",
             start_up: "NewWave Corporation",
@@ -171,10 +138,7 @@ export default async (models) => {
         },
         {
             name: "Cancer Destroyer",            //12th project (health)
-            presentation: loremIpsum({
-                count: 4,
-                units: "paragraph"
-            }),
+            presentation: "Our mission is to revolutionize cancer treatment through an innovative pill. With a team of dedicated experts, we aim to combat cancer and enhance patient outcomes. Through precision medicine and cutting-edge research, we empower individuals in their fight against cancer. Join us on this transformative journey towards a future where cancer is conquered, one pill at a time. Stay tuned for breakthrough advancements and hope with Cancer Destroyer by Elevance Health.",
             location: "Indianapolis, Indiana",
             date: "20/06/2023",
             start_up: "Elevance Health",
@@ -183,10 +147,7 @@ export default async (models) => {
         },
         {
             name: "Doriano Crackers",            //13th project (food)
-            presentation: loremIpsum({
-                count: 4,
-                units: "paragraph"
-            }),
+            presentation:"This exciting food project aims to produce delectable and crispy crackers for your snacking pleasure. With a focus on quality ingredients and exceptional taste, Doriano Crackers will elevate your snack time experience. Join us on this flavorful journey as we combine tradition with innovation to create the perfect balance of crunch and flavor. Stay tuned for a delightful assortment of Doriano Crackers that will tantalize your taste buds and leave you craving for more.",
             location: "Treviso, Italy",
             date: "21/03/2013",
             start_up: "Doria",
@@ -195,10 +156,7 @@ export default async (models) => {
         },
         {
             name: "Knox",            //14th project (IT)
-            presentation: loremIpsum({
-                count: 4,
-                units: "paragraph"
-            }),
+            presentation: "This cutting-edge IT project is dedicated to developing a state-of-the-art security framework. With a focus on safeguarding digital assets and protecting user privacy, Knox is designed to provide robust security solutions for businesses and individuals alike. Join us on this innovative journey as we redefine digital security, offering peace of mind and enhanced protection in an increasingly interconnected world. Stay tuned for the latest advancements in digital security with the Knox project by Samsung. ",
             location: "Seoul, South Korea",
             date: "01/03/2018",
             start_up: "Samsung",
@@ -207,10 +165,7 @@ export default async (models) => {
         },
         {
             name: "Paracetamolo",            //15th project (health)
-            presentation: loremIpsum({
-                count: 4,
-                units: "paragraph"
-            }),
+            presentation:"This vital health project aims to produce an analgesic syrup that provides effective relief from pain and fever. With a commitment to quality and patient well-being, Paracetamolo is designed to deliver fast and reliable relief for individuals of all ages. Join us on this journey to promote better health and well-being as we strive to improve the lives of those in need. Stay tuned for the launch of Paracetamolo, the trusted analgesic syrup by Angelini Industries.",
             location: "Rome, Italy",
             date: "17/05/2014",
             start_up: "Angelini Industries",
