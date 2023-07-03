e2fd8fbeeb240e75120e8d535daace0325a9edef
Alessio Alfonsi <61656683+gudea31@users.noreply.github.com>
2023-07-03 20:52:12 +0200

    Contact Us page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,84 +1,131 @@
-<!--
-    Contact Page
--->
 <template>
     <main>
-        <main>
-            <p>
-                This are our contacts:
-                <ul>
-                    <li>E-mail: mail@shelter.it</li>
-                    <li>Tel: 053684864351</li>
-                </ul>
-            </p>
-            <form>
-                <div>
-                    <label for = "name">Name & Surname</label>
-                    <input type = "text" id = "name" placeholder = "Name & Surname">
-                </div>
-                <div>
-                    <label for = "mail">E-mail</label>
-                    <input type = "email" id = "mail" placeholder = "E-mail">
-                </div>
-                <div id = "message-div">
-                    <label for = "message">Message</label>
-                    <textarea id = "message" placeholder = "Write your message here"></textarea>  
-                </div>
-                <button>Send Message</button>
-            </form>
-        </main>
+      <div class="contact-container">
+        <div class="form-wrapper">
+          <form>
+            <h2 class="form-title">Tell us your idea!</h2>
+            <div class="form-group">
+              <label for="name">Name</label>
+              <input type="text" id="name" placeholder="Name" />
+            </div>
+            <div class="form-group">
+              <label for="surname">Surname</label>
+              <input type="text" id="surname" placeholder="Surname" />
+            </div>
+            <div class="form-group">
+              <label for="mail">E-mail</label>
+              <input type="email" id="mail" placeholder="E-mail" />
+            </div>
+            <div class="form-group">
+              <label for="company">Company</label>
+              <input type="text" id="company" placeholder="Company" />
+            </div>
+            <div class="form-group" id="message">
+              <label for="message">Message</label>
+              <textarea id="message" placeholder="Write your message here"></textarea>
+            </div>
+            <button class="submit-button">Send Message</button>
+          </form>
+        </div>
+        <div class="contact-info">
+          <h2 class="info-title">Contact Information</h2>
+          <ul>
+            <li>E-mail: mail@shelter.it</li>
+            <li>Tel: 053684864351</li>
+          </ul>
+        </div>
+      </div>
     </main>
-</template>
+  </template>
+  
+  <style>
 
-<style>
-
+    .contact-container {
+      display: flex;
+      justify-content: space-between;
+      width: 800px;
+      padding: 20px;
+      background-color: transparent;
+    }
+  
+    .form-wrapper {
+      flex: 2;
+      border: 2px solid #333;
+      border-radius: 5px;
+      background-color: #5b7470;
+    }
+  
     form {
-        width: 600px;
-        display: flex;
-        flex-direction: column;
-        padding: 15px;
-        border: 2px rgb(255, 102, 0) dashed;
-        border-radius: 5px;
-        row-gap: 20px;
+      padding-right: 20px;
     }
-
-    form div {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
+  
+    .form-title {
+      font-size: 24px;
+      margin-bottom: 20px;
+      margin-left: 30px;
+      color: #b0bfc2;
     }
-
-    textarea {
-        width: 99%;
-        min-height: 5.1em;
-        resize: vertical;
+  
+    .form-group {
+      margin-bottom: 20px;
+      margin-left: 30px;
+      margin-right: 30px;
     }
-
-    input {
-        width: 60%;
-    }
-
+  
     label {
-        font-size: 13pt;
-        font-weight: bold;
+      display: block;
+      font-size: 16px;
+      font-weight: bold;
+      color: #b0bfc2;
+      margin-bottom: 5px;
+    }
+  
+    input,
+    textarea {
+      width: 100%;
+      padding: 10px;
+      font-size: 16px;
+      border-radius: 5px;
+      border: 1px solid #ccc;
+      border: none;
+      border-bottom: 2px solid #b0bfc2;
+      background-color: transparent;
+    }
+  
+    textarea {
+      resize: vertical;
+      min-height: 100px;
+    }
+  
+    .submit-button {
+      background-color: #333;
+      color: #fff;
+      border: none;
+      padding: 10px 20px;
+      font-size: 16px;
+      border-radius: 5px;
+      cursor: pointer;
+    }
+  
+    .contact-info {
+      flex: 1;
+      padding-left: 20px;
+    }
+  
+    .info-title {
+      font-size: 24px;
+      margin-bottom: 20px;
+    }
+  
+    ul {
+      list-style-type: none;
+      padding: 0;
+      margin: 0;
+    }
+  
+    li {
+      margin-bottom: 10px;
     }
 
-    #message-div {
-        flex-direction: column;
-    }
-
-    @media screen and (max-width: 650px) {
-        form div {
-            flex-direction: column;
-        }
-
-        input {
-            width: 100%;
-        }
-        
-        form {
-            width: 90%;
-            min-width: 400px;
-        }
-    }
-</style>
+    
+  </style>
