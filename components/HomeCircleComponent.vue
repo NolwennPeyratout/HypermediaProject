1828e2ccbad9b280fe5b0f2d3dc1e9e2635f5434
riccardogianni <riccardo1.gianni@mail.polimi.it>
2023-07-04 12:51:06 +0200

    Home page updated

---
+++ components/HomeCircleComponent.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="home-circle-container">
+      <NuxtLink :to="link">
+        <div class="home-circle">
+          <img :src="imgUrl" alt="Immagine del topic">
+        </div>
+      </NuxtLink>
+      <p class="home-title-center">{{ title }}</p>
+    </div>
+  </template>
+  
+
+<script setup>
+    const props = defineProps(['title', 'link','imgUrl'])
+</script>
+
+<style>
+.home-circle-container {
+  text-align: center;
+  margin: 40px;
+}
+
+.home-circle {
+  position: relative;
+  display: inline-block;
+  box-shadow: 2px 2px 4px #252525;
+  width: 250px;
+  height: 250px;
+  border-radius: 50%;
+  overflow: hidden;
+  transition: transform 0.3s;
+}
+
+.home-circle:hover {
+  transform: scale(1.1);
+  box-shadow: 4px 4px 8px #252525;
+}
+
+.home-circle img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.topic-name {
+  margin-top: 10px;
+}
+
+.topic-link {
+  display: block;
+  width: 100px;
+  height: 100px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 999;
+}
+.home-title-center{
+  text-align: center;
+  color: #252525;
+  font-size: 22px;
+}
+
+</style>  
---
+++ components/HomeRectangleComponent.vue
@@ -0,0 +1,70 @@
+<!-- TO BE FINISHED -->
+<template>
+  <div class="homeRect-container">
+    <NuxtLink :to="link" class="homeRect-link">
+      <div class="homeRect">
+          <img class="homeRect-img" :src="imgUrl" :alt="title">
+          <p id="homeRect-title">{{ title }}</p>
+          <p class="homeRect-subtitle">{{ subtitle }}</p>
+        </div>
+    </NuxtLink>
+
+  </div>
+</template>
+
+
+<script setup>
+  const props = defineProps(['title', 'link','imgUrl','subtitle'])
+</script>
+
+<style>
+.homeRect-container {
+  text-align: center;
+  width: 20%;
+  margin-bottom: 4%;
+}
+
+.homeRect {
+  position: relative;
+  display: inline-grid;
+}
+
+.homeRect img {
+  width: 100%;
+  object-fit: cover;
+}
+
+.topic-name {
+  margin-top: 10px;
+}
+
+.topic-link {
+  display: block;
+  width: 100px;
+  height: 100px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 999;
+}
+
+#homeRect-title{
+  margin-block-end: 0.1em;
+  text-align: start;
+  color: #252525;
+  font-size: 2em;
+  font-weight: bold;
+  background-color: white;
+  margin-top: 0%;
+  padding: 3%;
+}
+
+.homeRect-link{
+  text-decoration: none;
+}
+.homeRect-subtitle{
+  text-align: start;
+  background-color: white;
+}
+
+</style>  
