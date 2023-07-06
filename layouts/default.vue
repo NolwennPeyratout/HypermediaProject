bbf87f88c6073911bc6e8773884c33a0f503bd79
Alessio Alfonsi <61656683+gudea31@users.noreply.github.com>
2023-07-06 17:13:15 +0200

    fixed breadcrumbs

--- layouts/default.vue
+++ layouts/default.vue
@@ -86,21 +86,4 @@ export default defineNuxtComponent({
       text-decoration: underline;
     }
 
-    @media (max-width: 600px) {
-    .breadcrumb {
-      position: static;
-      margin-top: 0;
-      padding: 0.5rem;
-    }
-
-    .item {
-      display: block;
-      margin-bottom: 0.5rem;
-    }
-
-    .item:after {
-      content: '';
-    }
-  }
-
 </style>
