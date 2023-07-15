2a521e185c8d9cc5c1505a89bbd264531dc07c08
Timothée Poulain <nolwenn.peyratout@etu.univ-cotedazur.fr>
2023-07-15 22:57:19 +0200

    Fix add in id page

--- server/api/areas/[id].js
+++ server/api/areas/[id].js
@@ -7,7 +7,8 @@ export default defineEventHandler(async (event) => {
     const ret=[];
     const { data1, error }= await client.from('area').select("name,description").eq('name', name).limit(1).single()
     const { data2, error1 }= await client.from('project').select("name,presentation,location,date,start_up,product_service,relevant").eq("project_name","name").eq("area_name",name)
-    ret[0]=data1;
+    ret.push(data1);
+    ret.push(data2);
     ret[1]=data2;
     if(error | error1) {
         throw createError({statusCode: 400, statusMessage: error.message})
--- server/api/team/[id].js
+++ server/api/team/[id].js
@@ -9,9 +9,9 @@ export default defineEventHandler(async (event) => {
     const { data2, error1 }= await client.from('supervise').select("person_name,project_name")
     //supabase do the left join query automatically
     const { data3, error2 }= await client.from('concerns').select("area_name").eq('person_name',name)
-    ret[0]=data1;
-    ret[1]=data2;
-    ret[2]=data3;
+    ret.push(data1);
+    ret.push(data2);
+    ret.push(data3);
     if(error | error1 | error2) {
         throw createError({statusCode: 400, statusMessage: error.message})
     }
