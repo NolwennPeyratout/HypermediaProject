be3dfed0449ccc1ea2fac48cc5f711e348b96384
Timothée Poulain <nolwenn.peyratout@etu.univ-cotedazur.fr>
2023-07-15 17:48:42 +0200

    Change the url in the component homecircleComponent

--- components/HomeCircleComponent.vue
+++ components/HomeCircleComponent.vue
@@ -13,7 +13,7 @@ PROPS:
     <div class="home-circle-container">
       <NuxtLink :to="link">
         <div class="home-circle">
-          <img :src="imgUrl" alt="Immagine del topic">
+          <img :src="'~/assets/img/'+imgUrl" alt="Immagine del topic">
         </div>
       </NuxtLink>
       <p class="home-title-center">{{ title }}</p>
