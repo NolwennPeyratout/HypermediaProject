e5b086cd2e2d1139c8d6eeb8c40be06390baab6f
Timothée Poulain <nolwenn.peyratout@etu.univ-cotedazur.fr>
2023-07-16 12:51:56 +0200

    Change query for food

--- server/api/projects/byarea/Food/index.js
+++ server/api/projects/byarea/Food/index.js
@@ -4,7 +4,10 @@ export default defineEventHandler(async (event) => {
     const name = event.context.params.id
     
     const client = serverSupabaseClient(event)
-    const { data, error }= await client.from('project').select("name,presentation,location,date,start_up,product_service,relevant")
+    const { data, error }= await client.from('project').select('name, presentation, location, date, start_up, product_service, relevant')
+    .filterExists(
+      supabase.from('concern').select('*').filter('concern.area_name', 'eq', name).filter('concern.project_name', 'eq', 'project.name')
+    );
     
 
     if(error) {
