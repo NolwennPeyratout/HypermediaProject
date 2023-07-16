a20a57522b3458420e4c4ebaa8bca761439e77dc
Timothée Poulain <nolwenn.peyratout@etu.univ-cotedazur.fr>
2023-07-16 11:36:44 +0200

    test query on team id

--- server/api/team/[id].js
+++ server/api/team/[id].js
@@ -5,7 +5,7 @@ export default defineEventHandler(async (event) => {
     
     const client = serverSupabaseClient(event)
     //const data=[];
-    const { data, error }= await client.from('person').select("name,role,cv,introduction").eq('name', name).limit(1).single()
+    const { data, error }= await client.from('person').select("*").eq('name', name)
     //const { ret2, error1 }= await client.from('supervise').select("person_name,project_name")
     //supabase do the left join query automatically
     //const { ret3, error2 }= await client.from('concerns').select("area_name").eq('person_name',name)
