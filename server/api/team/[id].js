046684d194d63890c74fada60a2af749ea7429a2
Timothée Poulain <nolwenn.peyratout@etu.univ-cotedazur.fr>
2023-07-16 11:11:13 +0200

    show data in team id

--- server/api/team/[id].js
+++ server/api/team/[id].js
@@ -4,17 +4,17 @@ export default defineEventHandler(async (event) => {
     const name = event.context.params.id
     
     const client = serverSupabaseClient(event)
-    const ret=[];
-    const { data1, error }= await client.from('person').select("name,role,cv,introduction").eq('name', name).limit(1).single()
-    const { data2, error1 }= await client.from('supervise').select("person_name,project_name")
+    const data=[];
+    const { ret1, error }= await client.from('person').select("name,role,cv,introduction").eq('name', name).limit(1).single()
+    const { ret2, error1 }= await client.from('supervise').select("person_name,project_name")
     //supabase do the left join query automatically
-    const { data3, error2 }= await client.from('concerns').select("area_name").eq('person_name',name)
-    ret.push(data1);
-    ret.push(data2);
-    ret.push(data3);
+    const { ret3, error2 }= await client.from('concerns').select("area_name").eq('person_name',name)
+    data.push(ret1);
+    data.push(ret2);
+    data.push(ret3);
     if(error | error1 | error2) {
         throw createError({statusCode: 400, statusMessage: error.message})
     }
 
-    return ret
+    return data
 })
