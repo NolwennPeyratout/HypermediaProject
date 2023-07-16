5bb9d5e58edbd01a4a637f66c9a74415c244002e
Timothée Poulain <nolwenn.peyratout@etu.univ-cotedazur.fr>
2023-07-16 11:46:36 +0200

    Update [id].vue

--- pages/team/[id].vue
+++ pages/team/[id].vue
@@ -18,7 +18,7 @@
             <p id="cv">{{ data[0].cv }}</p>
             -->
           </div>
-          {{ console.log(data) }}
+          {{ data }}
         </div>
         <!--This part is shown only if the person is the supervisor of one project, and we show it to avoid the carousel
         <div v-if="data[1].length == 1" id="container-project">
