d1017eacf8665941fae5ba22affeb1f34e946d81
Timothée Poulain <nolwenn.peyratout@etu.univ-cotedazur.fr>
2023-07-16 19:54:42 +0200

    change query for project id

--- server/api/projects/[id].js
+++ server/api/projects/[id].js
@@ -1,9 +1,9 @@
 import { serverSupabaseClient } from '#supabase/server'
 
 export default defineEventHandler(async (event) => {
-    
+    const name = event.context.params.id
     const client = serverSupabaseClient(event)
-    const { data, error }= await client.from('project').select("name,presentation,location,date,start_up,product_service,relevant")
+    const { data, error }= await client.from('project').select("*").eq(name,'name').limit(1).single()
     
     if(error ) {
         throw createError({statusCode: 400, statusMessage: error.message})
