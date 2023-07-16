84556152afe0d948d8eb06b934f8af48370637be
Timothée Poulain <nolwenn.peyratout@etu.univ-cotedazur.fr>
2023-07-16 21:51:36 +0200

    see the data

--- server/api/projects/[id].js
+++ server/api/projects/[id].js
@@ -1,9 +1,9 @@
 import { serverSupabaseClient } from '#supabase/server'
 
 export default defineEventHandler(async (event) => {
-    const id = event.context.params.id ;
+    const data = event.context.params.id ;
     const client = serverSupabaseClient(event);
-    const { data, error }= await client.from('project').select("name,presentation,location,date,start_up,product_service,relevant").ilike('name',id);
+   // const { data, error }= await client.from('project').select("name,presentation,location,date,start_up,product_service,relevant").ilike('name',id);
     
     if(error ) {
         throw createError({statusCode: 400, statusMessage: error.message})
