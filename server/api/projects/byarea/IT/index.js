1fac2c17be28567048bffc57c5c7fc9068514ab4
Timothée Poulain <nolwenn.peyratout@etu.univ-cotedazur.fr>
2023-07-16 20:25:26 +0200

    Test for the project by area

--- server/api/projects/byarea/IT/index.js
+++ server/api/projects/byarea/IT/index.js
@@ -1,10 +1,10 @@
 import { serverSupabaseClient } from '#supabase/server'
 
 export default defineEventHandler(async (event) => {
-    const name = event.context.params.id
+    const id = event.context.params.id
     
     const client = serverSupabaseClient(event)
-    const { data, error }= await client.rpc('get_projects_with_concerns', { areaid: name });
+    const { data, error }= await client.rpc('get_projects_with_concerns', {  areaid:'IT' });
 
     if(error) {
         throw createError({statusCode: 400, statusMessage: error.message})
