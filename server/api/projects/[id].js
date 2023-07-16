603c5dd7a3021d2fc466d78d2d84124b278b5bbe
Timothée Poulain <nolwenn.peyratout@etu.univ-cotedazur.fr>
2023-07-16 20:09:02 +0200

    Test the query

--- server/api/projects/[id].js
+++ server/api/projects/[id].js
@@ -3,7 +3,7 @@ import { serverSupabaseClient } from '#supabase/server'
 export default defineEventHandler(async (event) => {
     const name = event.context.params.id ;
     const client = serverSupabaseClient(event);
-    const { data, error }= await client.from('project').select("name,presentation,location,date,start_up,product_service,relevant")
+    const { data, error }= await client.from('project').select("name,presentation,location,date,start_up,product_service,relevant").eq('name','Ringo');
     
     if(error ) {
         throw createError({statusCode: 400, statusMessage: error.message})
