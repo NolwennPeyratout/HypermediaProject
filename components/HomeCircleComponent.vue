eda9f477e765f66d90ccff29feeeea27f13b0f28
Timothée Poulain <nolwenn.peyratout@etu.univ-cotedazur.fr>
2023-07-16 16:28:02 +0200

    change the way of getting picture

--- components/HomeCircleComponent.vue
+++ components/HomeCircleComponent.vue
@@ -13,7 +13,7 @@ PROPS:
     <div class="home-circle-container">
       <NuxtLink :to="link">
         <div class="home-circle">
-          <img :src="imgUrl" alt="Immagine del topic">
+          <img :src="require(imgUrl)" alt="Immagine del topic">
         </div>
       </NuxtLink>
       <p class="home-title-center">{{ title }}</p>
