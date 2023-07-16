002fbe040853596b8a03eb3bc869078c3fa0083d
Timothée Poulain <nolwenn.peyratout@etu.univ-cotedazur.fr>
2023-07-16 22:36:12 +0200

    Update [id].js

--- server/api/areas/[id].js
+++ server/api/areas/[id].js
@@ -1,10 +1,10 @@
 import { serverSupabaseClient } from '#supabase/server'
 
 export default defineEventHandler(async (event) => {
-    const data = event.context.params.id
+    const name = event.context.params.id
     
     const client = serverSupabaseClient(event)
-    //const { data , error }= await client.from('area').select("name,description").eq('name',name).limit(1).single()
+    const { data , error }= await client.from('area').select("name,description").eq('name',name).limit(1).single()
     
     if(error  ) {
         throw createError({statusCode: 400, statusMessage: error.message})
