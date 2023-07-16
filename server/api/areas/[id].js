202918e2f0873024bab55ab5ce937a9de7a1d439
Timothée Poulain <nolwenn.peyratout@etu.univ-cotedazur.fr>
2023-07-16 12:08:20 +0200

    Update [id].js

--- server/api/areas/[id].js
+++ server/api/areas/[id].js
@@ -5,7 +5,7 @@ export default defineEventHandler(async (event) => {
     
     const client = serverSupabaseClient(event)
     //const ret=[];
-    const { ret, error }= await client.from('area').select("name,description").eq('name', name).limit(1).single()
+    const { ret, error }= await client.from('area').select("name,description")
     //const { data2, error1 }= await client.from('project').select("name,presentation,location,date,start_up,product_service,relevant").eq("project_name","name").eq("area_name",name)
     //ret.push(data1);
     //ret.push(data2);
