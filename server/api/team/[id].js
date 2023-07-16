df5ed18573dd0c97d71ad9284d25be27426c62aa
Timothée Poulain <nolwenn.peyratout@etu.univ-cotedazur.fr>
2023-07-16 18:44:58 +0200

    Change the query teamid

--- server/api/team/[id].js
+++ server/api/team/[id].js
@@ -1,13 +1,15 @@
 import { serverSupabaseClient } from '#supabase/server'
 
 export default defineEventHandler(async (event) => {
-    const name = event.context.params.id
+    const id = event.context.params.id
+    const [surname, name] = id.split(' ');
+    const fullname=surname+" "+name;
     
     const client = serverSupabaseClient(event)
     //const data=[];
     //ilike is use to perform a case-intensive comparison, so an exact match between name and name.
     //const { data, error }= await client.from('person').select("*").filter('name','ilike', name);
-    const { data, error }= await client.from('person').select("*")
+    const { data, error }= await client.from('person').select("*").eq(fullname,'name')
     //const { ret2, error1 }= await client.from('supervise').select("person_name,project_name")
     //supabase do the left join query automatically
     //const { ret3, error2 }= await client.from('concerns').select("area_name").eq('person_name',name)
