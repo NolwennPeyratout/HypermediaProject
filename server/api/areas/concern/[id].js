866272ba996bd8f385eca52091ce5873ac9fd5ac
Timothée Poulain <nolwenn.peyratout@etu.univ-cotedazur.fr>
2023-07-17 10:50:06 +0200

    Change queries in area id

--- server/api/areas/concern/[id].js
+++ server/api/areas/concern/[id].js
@@ -4,8 +4,7 @@ export default defineEventHandler(async (event) => {
     const name = event.context.params.id
     
     const client = serverSupabaseClient(event)
-    const { data , error }= await client
-        .rpc('get_projects_with_concerns', { areaid: name });
+    const { data, error }= await client.from('area').select("name,description").eq('name',name).limit(1).single()
     if(error  ) {
         throw createError({statusCode: 400, statusMessage: error.message})
     }
