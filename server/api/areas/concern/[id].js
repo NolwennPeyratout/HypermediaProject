897402d88bbc7426a91381b6656d289ed0e7fd41
Timothée Poulain <nolwenn.peyratout@etu.univ-cotedazur.fr>
2023-07-17 13:50:12 +0200

    test query

--- server/api/areas/concern/[id].js
+++ server/api/areas/concern/[id].js
@@ -6,8 +6,9 @@ export default defineEventHandler(async (event) => {
   const supabase = createClient('https://fzgzmacqofehsdojhdvf.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ6Z3ptYWNxb2ZlaHNkb2poZHZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODk0MjYxMDgsImV4cCI6MjAwNTAwMjEwOH0.OeimVTBSoYYug9-V_zgFtVlL3-X9a0t3uUeJdBUMedM');
   
   try {
+    //const { data, error } = await supabase.from('area').select('name,description')
     const { data, error } = await supabase
-      .from('area').select("name,description")
+      .rpc('get_projects_with_concerns', { areaid: areaId });
     if (error) {
       throw new Error(error.message);
     }
