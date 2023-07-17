8f86638e5b8b20c21612ba154036196182c6fa10
Timothée Poulain <nolwenn.peyratout@etu.univ-cotedazur.fr>
2023-07-17 13:19:39 +0200

    Change query area id

--- server/api/areas/concern/[id].js
+++ server/api/areas/concern/[id].js
@@ -1,13 +1,21 @@
-import { serverSupabaseClient } from '#supabase/server'
+import { createClient } from '@supabase/supabase-js';
 
 export default defineEventHandler(async (event) => {
-    const name = event.context.params.id
-    
-    const client = serverSupabaseClient(event)
-    const { data, error }= await client.from('area').select("name,description")
-    if(error  ) {
-        throw createError({statusCode: 400, statusMessage: error.message})
+  const id = event.context.params.id;
+
+  const supabase = createClient('https://fzgzmacqofehsdojhdvf.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ6Z3ptYWNxb2ZlaHNkb2poZHZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODk0MjYxMDgsImV4cCI6MjAwNTAwMjEwOH0.OeimVTBSoYYug9-V_zgFtVlL3-X9a0t3uUeJdBUMedM');
+  
+  try {
+    const { data, error } = await supabase
+      .from('area').select("name,description")
+    if (error) {
+      throw new Error(error.message);
     }
 
-    return data
+    return {
+      data,
+    };
+  } catch (error) {
+    throw new Error(error.message);
+  }
 })
