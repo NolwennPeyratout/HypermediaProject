3ca961af7ab37bba340d3ced3a022036e308dcf4
Alessio Alfonsi <61656683+gudea31@users.noreply.github.com>
2023-07-18 21:33:44 +0200

    w

--- components/ProjectCardExtended.vue
+++ components/ProjectCardExtended.vue
@@ -38,7 +38,7 @@ PROPS:
     const props = defineProps(['title', 'imgUrl', 'link', 'product', 'date', 'location']);
     const modifiedTitle = computed(() => props.title.replace(/\+/g, ' '));
     const u = "" + props.imgUrl;
-    const imageUrl = new URL(u, import.meta.url);
+    const imageUrl = new URL('~/assets/img/projects/BioVegan+Smoothies1.jpg', import.meta.url);
       
     </script>
     
